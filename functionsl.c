c0f1974f2ac10d8320d4bdd9a83a648b755ad64e
edwinb-ai <e.a.bedollamontiel@uu.nl>
2024-01-28 15:22:32 +0100

    General changes, formatting and got rid of OpenMP pragmas

--- functionsl.c
+++ functionsl.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
-#include <omp.h>
+// #include <omp.h>
 #include "random_s.h"
 #include "functionsl.h"
 
-void iniconf(double* x, double* y, double* z, double* w, double rho, double rc, int num_part)
-{
+void iniconf(double* x, double* y, double* z, double* w, double rho, double rc, int num_part) {
     // Definir la distancia segun la densidad
     double dist = pow(rho, -1.0/4.0);
 
@@ -16,29 +15,25 @@ void iniconf(double* x, double* y, double* z, double* w, double rho, double rc, 
     z[0] = -rc + (dist / 2.0);
     w[0] = -rc + (dist / 2.0);
 
-    for (int i = 0; i < num_part; i++)
-    {
+    for (int i = 0; i < num_part; i++) {
         x[i+1] = x[i] + dist;
         y[i+1] = y[i];
         z[i+1] = z[i];
         w[i+1] = w[i];
 
-        if (x[i+1] > rc)
-        {
+        if (x[i+1] > rc) {
             x[i+1] = -rc + (dist / 2.0);
             y[i+1] += dist;
             z[i+1] = z[i];
             w[i+1] = w[i];
 
-            if (y[i+1] > rc)
-            {
+            if (y[i+1] > rc) {
                 x[i+1] = -rc + (dist / 2.0);
                 y[i+1] = -rc + (dist / 2.0);
                 z[i+1] += dist;
                 w[i+1] = w[i];
 
-                if (z[i+1] > rc)
-                {
+                if (z[i+1] > rc) {
                     x[i+1] = -rc + (dist / 2.0);
                     y[i+1] = -rc + (dist / 2.0);
                     z[i+1] = -rc + (dist / 2.0);
@@ -49,8 +44,7 @@ void iniconf(double* x, double* y, double* z, double* w, double rho, double rc, 
     }
 }
 
-double hardsphere(double r_pos)
-{
+double hardsphere(double r_pos) {
     double uij = 0.0;
 
     uij = (a_param/temp) * (pow(1.0/r_pos, lambda) - pow(1.0/r_pos, lambda-1.0));
@@ -61,8 +55,7 @@ double hardsphere(double r_pos)
 }
 
 double rdf_force(double* x, double* y, double* z, double*w, double* fx, double* fy, double* fz,
-double* fw, const int num_part, const double box_l)
-{
+double* fw, const int num_part, const double box_l) {
     // Parámetros
     double rc = box_l/2.0;
     double d_r = rc / nm;
@@ -72,11 +65,10 @@ double* fw, const int num_part, const double box_l)
     double fij = 0.0, wij = 0.0;
     double uij = 0.0, ener = 0.0;
     int nbin = 0;
-    size_t i = 0, j = 0;
+    int i = 0, j = 0;
 
     // Inicializar arreglos para la fuerza
-    for (i = 0; i < num_part; i++)
-    {
+    for (i = 0; i < num_part; i++) {
         fx[i] = 0.0;
         fy[i] = 0.0;
         fz[i] = 0.0;
@@ -84,10 +76,8 @@ double* fw, const int num_part, const double box_l)
     }
 
     // #pragma omp parallel for num_threads(3) default(shared) private(xij,yij,zij,wij,i,j,rij) reduction(+:ener)
-    for (i = 0; i < num_part; i++)
-    {
-        for (j = i+1; j < num_part-1; j++)
-        {
+    for (i = 0; i < num_part; i++) {
+        for (j = i+1; j < num_part-1; j++) {
             // Siempre inicializar en cero
             uij = 0.0;  
             fij = 0.0;  
@@ -105,17 +95,14 @@ double* fw, const int num_part, const double box_l)
             wij -= (box_l * round(wij/box_l));
 
             rij = sqrt(xij*xij + yij*yij + zij*zij + wij*wij);
-            if (rij < rc)
-            {
+            if (rij < rc) {
                 // Siempre se calcula la fuerza
-                if (rij < b_param)
-                {
+                if (rij < b_param) {
                     uij = hardsphere(rij);
                     fij = lambda*pow(1.0/rij, lambda+1.0) - (lambda-1.0)*pow(1.0/rij, lambda);
                     fij *= (a_param/temp);
                 }
-                else
-                {
+                else {
                     uij = 0.0;
                     fij = 0.0;
                 }
@@ -139,17 +126,15 @@ double* fw, const int num_part, const double box_l)
 
 void position(double* x, double* y, double* z, double* w, double* fx, double* fy,
 double* fz, double* fw, const double dtt,
-const double box_l, const int num_part, const int pbc)
-{
+const double box_l, const int num_part, const int pbc) {
     // Inicializar algunas variables
     double dx = 0.0;
     double dy = 0.0;
     double dz = 0.0;
     double dw = 0.0;
-    double sigma = sqrt(2.0*dtt);
+    const double sigma = sqrt(2.0*dtt);
 
-    for (int i = 0; i < num_part; i++)
-    {
+    for (int i = 0; i < num_part; i++) {
         dx = sigma * norm();
         dy = sigma * norm();
         dz = sigma * norm();
@@ -160,8 +145,7 @@ const double box_l, const int num_part, const int pbc)
         z[i] += fz[i]*dtt + dz;
         w[i] += fw[i]*dtt + dw;
 
-        if (pbc == 1)
-        {
+        if (pbc == 1) {
             x[i] -= (box_l * round(x[i]/box_l));
             y[i] -= (box_l * round(y[i]/box_l));
             z[i] -= (box_l * round(z[i]/box_l));
@@ -171,8 +155,7 @@ const double box_l, const int num_part, const int pbc)
 }
 
 void gr(double* x, double* y, double* z, double* w, double* g,
-const int num_part, const double box_l)
-{
+const int num_part, const double box_l) {
     // Parámetros
     double rc = box_l/2.0;
     double d_r = rc / nm;
@@ -182,10 +165,8 @@ const int num_part, const double box_l)
     double xij = 0.0, yij = 0.0, zij = 0.0, rij = 0.0;
     double wij = 0.0;
 
-    for (i = 0; i < num_part; i++)
-    {
-        for (j = i+1; j < num_part-1; j++)
-        {
+    for (i = 0; i < num_part; i++) {
+        for (j = i+1; j < num_part-1; j++) {
 
             // Contribucion de pares
             xij = x[j] - x[i];
@@ -201,11 +182,9 @@ const int num_part, const double box_l)
 
             rij = sqrt(xij*xij + yij*yij + zij*zij + wij*wij);
 
-            if (rij < rc)
-            {
+            if (rij < rc) {
                 nbin = (int)(rij/d_r) + 1;
-                if (nbin <= nm)
-                {
+                if (nbin <= nm) {
                     g[nbin] += 2.0;
                 }
             }
@@ -214,19 +193,15 @@ const int num_part, const double box_l)
 }
 
 void difusion(const int nprom, const int n_part, double* cfx, double* cfy, double* cfz,
-double* cfw, double* wt)
-{
+double* cfw, double* wt) {
     double dif2 = 0.0;
     double dx = 0.0, dy = 0.0, dz = 0.0, dw = 0.0;
     double aux2 = 0.0;
 
-    for (int i = 0; i < nprom; i++)
-    {
+    for (int i = 0; i < nprom; i++) {
         dif2 = 0.0;
-        for (int j = 0; j < nprom-i; j++)
-        {
-            for (int k = 0; k < n_part; k++)
-            {
+        for (int j = 0; j < nprom-i; j++) {
+            for (int k = 0; k < n_part; k++) {
                 dx = cfx[(j+i)*mp + k] - cfx[j*mp + k];
                 dy = cfy[(j+i)*mp + k] - cfy[j*mp + k];
                 dz = cfz[(j+i)*mp + k] - cfz[j*mp + k];
--- langevin_4D.c
+++ langevin_4D.c
@@ -4,8 +4,7 @@
 #include "functionsl.h"
 #include "pseudorng.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     // Archivos para trabajar
     FILE *f_iniconf;
     FILE *f_gr;
@@ -32,7 +31,7 @@ int main(int argc, char const *argv[])
     double l_caja = pow(n_part / rho, 1.0/4.0);
     double radio_c = l_caja/2.0;
     double dr = radio_c/nm;
-    double dq = M_PI/radio_c;
+    // double dq = M_PI/radio_c;
 
     // Mostrar información del sistema
     printf("El tamaño de la caja es: %f\n", l_caja);
@@ -73,23 +72,21 @@ int main(int argc, char const *argv[])
     // Configuración inicial
     iniconf(x, y, z, w, rho, radio_c, n_part);
     f_iniconf = fopen("conf_inicial.dat", "w");
-    for (int i = 0; i < n_part; i++)
-    {
+    for (int i = 0; i < n_part; i++) {
         fprintf(f_iniconf, "%.10f %.10f %.10f %.10f\n", x[i], y[i], z[i], w[i]);
     }
     fclose(f_iniconf);
     // Verificar que la energía es cero
-    double ener = 0.0;
+    double ener = 0.0, enerpart;
     ener = rdf_force(x, y, z, w, fx, fy, fz, fw, n_part, l_caja);
-    printf("E/N: %.10f\n", ener/((double)(n_part)));
+    enerpart = ener / ((double)(n_part));
+    printf("E/N: %.10f\n", enerpart);
 
     // Termalizar el sistema
     // Se puede utilizar una termalización anterior
-    if (thermal == 1)
-    {
+    if (thermal == 1) {
         f_final = fopen("final_conf.dat", "r");
-        for (int i = 0; i < n_part; i++)
-        {
+        for (int i = 0; i < n_part; i++) {
             fscanf(f_final, "%lf", &x[i]);
             fscanf(f_final, "%lf", &y[i]);
             fscanf(f_final, "%lf", &z[i]);
@@ -102,29 +99,25 @@ int main(int argc, char const *argv[])
         fclose(f_final);
     }
     // O crear una nueva
-    else
-    {
+    else {
         f_ener = fopen("energia.dat", "w");
         f_final = fopen("final_conf.dat", "w");
 
-        for (int i=0; i < nct; i++)
-        {
+        for (int i = 0; i < nct; i++) {
             position(x, y, z, w, fx, fy, fz, fw, d_tiempo, l_caja, n_part, 1);
             ener = rdf_force(x, y, z, w, fx, fy, fz, fw, n_part, l_caja);
-            if (i%10000 == 0)
-            {
-                printf("%d %.10f Thermal\n", i, ener/((double)(n_part)));
+            enerpart = ener / ((double)(n_part));
+            if (i%10000 == 0) {
+                printf("%d %.10f Thermal\n", i, enerpart);
             }
-            if (i%100 == 0)
-            {
-                fprintf(f_ener, "%d %.10f\n", i, ener/((double)(n_part)));
+            if (i%100 == 0) {
+                fprintf(f_ener, "%d %.10f\n", i, enerpart);
             }
         }
         fclose(f_ener);
 
         // Guardar la configuración final después de termalizar
-        for (int i=0; i < n_part; i++)
-        {
+        for (int i=0; i < n_part; i++) {
             fprintf(f_final, "%.10f %.10f %.10f %.10f %.10f %.10f %.10f %.10f\n",
             x[i], y[i], z[i], w[i], fx[i], fy[i], fz[i], fw[i]);
         }
@@ -133,19 +126,16 @@ int main(int argc, char const *argv[])
 
     // Calcular la g(r)
     int nprom = 0;
-    for (int i=0; i < ncp; i++)
-    {
+    for (int i = 0; i < ncp; i++) {
         position(x, y, z, w, fx, fy, fz, fw, d_tiempo, l_caja, n_part, 0);
         ener = rdf_force(x, y, z, w, fx, fy, fz, fw, n_part, l_caja);
-        if (i%10000 == 0)
-        {
-            printf("%d %.10f Average\n", i, ener/((double)(n_part)));
+        enerpart = ener / ((double)(n_part));
+        if (i%10000 == 0) {
+            printf("%d %.10f Average\n", i, enerpart);
         }
-        if (i%10 == 0)
-        {
+        if (i%10 == 0) {
             t[nprom] = d_tiempo*10.0*nprom;
-            for (int j = 0; j < n_part; j++)
-            {
+            for (int j = 0; j < n_part; j++) {
                 cfx[nprom*mp + j] = x[j];
                 cfy[nprom*mp + j] = y[j];
                 cfz[nprom*mp + j] = z[j];
@@ -160,11 +150,10 @@ int main(int argc, char const *argv[])
     double *r = calloc(nm, sizeof(double));
     double dv = 0.0;
     // double hraux = 0.0;
-    for (int i=1; i < nm; i++)
-    {
+    for (int i = 1; i < nm; i++) {
         r[i] = (i-1)*dr;
         dv = dr*2.0*pisq*pow(r[i], 3);
-        g[i] *= pow(l_caja, 4.0) / (pow(n_part, 2)*nprom*dv);
+        g[i] *= pow(l_caja, 4) / (pow(n_part, 2)*nprom*dv);
         h[i] = g[i] - 1.0;
         fprintf(f_gr, "%.10f %.10f %.10f\n", r[i], g[i], h[i]);
     }
@@ -173,8 +162,7 @@ int main(int argc, char const *argv[])
     // Llamar la función para el desplazamiento cuadrático medio
     difusion(nprom, n_part, cfx, cfy, cfz, cfw, wt);
     f_wt = fopen(argv[7], "w");
-    for (int i = 0; i < ncp/10; i++)
-    {
+    for (int i = 0; i < ncp/10; i++) {
         fprintf(f_wt, "%.10f %.10f\n", t[i], wt[i]);
     }
     fclose(f_wt);
